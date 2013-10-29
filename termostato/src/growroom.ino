5db029016e381e1788a32c043bab96d304f2891e
root <root@eeepc.alanza10.dyndns.org>
2013-10-29 17:44:07 +0100

    0.3

--- termostato/src/growroom.ino
+++ termostato/src/growroom.ino
@@ -1,17 +1,32 @@
 /*
  * control de growroom 
- * version 0.1 : temperatura am2301
+ * pin usados digitales 3 (relay termostato) 5 (temp superior)
+ * version 0.1 : temperatura am2301 1 termometro superior
+ * version 0.2 : control relay 
+ * version 0.3 : control relay termostato configurar maximo y margen 
  */
 
 #include <dht.h>
+#include <Rele.h>
 
 dht DHT;
 //para control y protocolo
-#define MSG_LEN  2
+#define MSG_LEN  6
 #define TEMP_HEADER 'T'
 #define TEMP_SUP 'S'
 #define TERMOSUP 5
+#define RELAY_HEADER  'R'
+#define RELAY_ON  'E'  
+#define RELAY_OFF  'A'
+#define RELAY_TERM 3
+#define CONFIG_HEADER  'C'
+#define CONFIG_MAX 'M'
+#define CONFIG_MARGEN 'R'
+#define DEF_MARGEN_INIT 10
+#define DEF_TEMP_MAX 35
 
+Rele termrele;
+int temp_max=DEF_TEMP_MAX,margen=DEF_MARGEN_INIT;
 
 void setup()
 {
@@ -19,6 +34,12 @@ void setup()
   Serial.println("CONTROL GROWROOM");
   Serial.println("********************");
   Serial.println("Type,\tstatus,\tHumidity (%),\tTemperature (C)");
+  //setReles
+  termrele.setRele(1);
+  termrele.setPin(RELAY_TERM);
+  termrele.setOutput();
+  termrele.releOff();
+ 
 }
 
 void loop()
@@ -27,6 +48,7 @@ void loop()
   {
 	processMessage();
   }
+  termostato();
   delay(1000);
 }
 
@@ -34,10 +56,16 @@ void loop()
 
 void processMessage(){
  
-  while(Serial.available() >=  MSG_LEN ){  
-	  char cmd = Serial.read() ;  
+  while(Serial.available() >=  MSG_LEN ){ 
+	  char command[MSG_LEN-1]; 
+	  char cmd = Serial.read() ; 
+	  
 	  if( cmd == TEMP_HEADER ) { 
 		  char c = Serial.read();
+		  //resto chars comando
+	       for(int i=0; i < MSG_LEN -2; i++){   
+	        command[i] = Serial.read();           
+	      }
 		  int pos;
 		  if(c==TEMP_SUP){
 			  // READ DATA Sup
@@ -66,7 +94,60 @@ void processMessage(){
 		  Serial.print(",\t");
 		  Serial.println(DHT.temperature, 1);
 	  }
-  }  
+	  
+	  if( cmd == RELAY_HEADER ) { 
+		  char c = Serial.read();
+		  //resto chars comando
+	       for(int i=0; i < MSG_LEN -2; i++){   
+	        command[i] = Serial.read();           
+	      }
+		  int pos;
+		  if(c==RELAY_ON){
+			  termrele.releOn();
+		  }else if(c==RELAY_OFF){
+			  termrele.releOff();
+		  }
+	  } 
+	  if( cmd == CONFIG_HEADER ) { 
+		  char c = Serial.read();
+		  char c2 = Serial.read();
+		  //resto chars comando
+	       for(int i=0; i < MSG_LEN -3; i++){   
+	        command[i] = Serial.read();           
+	      }
+		  int pos;
+		  if(c==CONFIG_MAX && c2==CONFIG_MARGEN){
+			  Serial.print("config max temp");
+			  Serial.print(temp_max);
+			  Serial.print(" margen ");
+			  Serial.println(margen);
+		  }
+	  }
+  }
 }
 
-
+void termostato(){
+	int chk = DHT.read21(TERMOSUP);
+	  switch (chk)
+	  {
+	    case DHTLIB_OK:  
+			Serial.print("OK,\t"); 
+			break;
+	    case DHTLIB_ERROR_CHECKSUM: 
+			Serial.print("Checksum error,\t"); 
+			break;
+	    case DHTLIB_ERROR_TIMEOUT: 
+			Serial.print("Time out error,\t"); 
+			break;
+	    default: 
+			Serial.print("Unknown error,\t"); 
+			break;
+	  }
+	  // DISPLAY DATA
+	  Serial.print(DHT.humidity, 1);
+	  Serial.print(",\t");
+	  Serial.println(DHT.temperature, 1);
+	  if(DHT.temperature>=temp_max && !termrele.isOn())termrele.releOn();
+	  else if(DHT.temperature<=temp_max-margen && termrele.isOn()) termrele.releOff();
+	
+}
